598a1c1835ac4fc69cd894fef3b491a7e1469493
Giuliana <giuliana.torres05@gmail.com>
2022-07-16 18:09:37 -0600

    Answered part one questions.

---
+++ app.js
@@ -0,0 +1,36 @@
+/* Part One
+What is HTTP?
+  HTTP is an application protocol or standard model to interact with the web page.
+
+What is a URL?
+  A URL is a web address. It references the location of the web resource it is trying to retrieve.
+
+What is DNS?
+  DNS takes the host name found in the url to the corresponding IP address.
+
+What is a query string?
+  A query string is in the URL and it sets the parameter of the resource it is looking for.
+
+What are two HTTP verbs and how are they different?
+  GET - does not change the data on the server
+  POST - changes the data on the server
+
+What is an HTTP request?
+  An HTTP request is a request to get a resource on the server. Client to server.
+
+What is an HTTP response?
+  The HTTP response is that resource if its successful along with a status. Server to client.
+
+What is an HTTP header? Give a couple examples of request and response headers you have seen.
+  A request header shows what to access and a response header gives back more details on what you get back.
+  The request header shows the host and what link the host is referring to.
+  The response header has date of when it was last-modified.
+
+What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser?
+  Using DNS it finds the IP address from the hostname.
+  Requests are made in which more details are specified in the request headers.
+  The requests are made to the server, if the resources are found elsewhere than the server makes that request to
+  another server and retrieves it for the user.
+  The server sends a response to the client.
+*/
+  
